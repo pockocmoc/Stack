fc3736cfffe9c616b26789b76436cf1f08fd3950
pockocmocWork <pockocmoc@inbox.ru>
2022-08-05 07:25:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stack<Integer> myStack1 = new Stack<>();
+        Stack<Integer> myStack2 = new Stack<>();
+
+        for (int i = 0; i < 10; i++) {
+            myStack1.push(i);
+
+        }
+        for (int i = 10; i < 20; i++) {
+            myStack2.push(i);
+
+        }
+
+        System.out.println("Содержимое стека myStack1: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(myStack1.pop());
+
+        }
+        System.out.println("Содержимое стека myStack2: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(myStack2.pop());
+
+
+        }
+    }
+}
